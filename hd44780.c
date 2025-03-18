c692a9882afdbb15806775dd345f33fb9c048580
ddening <44156662+ddening@users.noreply.github.com>
2025-03-18 15:27:23 +0100

    feat: Added analog mode

---
+++ hd44780.c
@@ -0,0 +1,414 @@
+/*************************************************************************
+* Title		: hd44780.c
+* Author	: Dimitri Dening
+* Created	: 08.03.2025 22:06:32
+* Software	: Microchip Studio V7
+* Hardware	: Atmega2560
+* License	: MIT License
+* Usage		: see Doxygen manual
+*
+*       Copyright (C) 2025 Dimitri Dening
+*
+*       Permission is hereby granted, free of charge, to any person obtaining a copy
+*       of this software and associated documentation files (the "Software"), to deal
+*       in the Software without restriction, including without limitation the rights
+*       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*       copies of the Software, and to permit persons to whom the Software is
+*       furnished to do so, subject to the following conditions:
+*
+*       The above copyright notice and this permission notice shall be included in all
+*       copies or substantial portions of the Software.
+*
+*       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*       SOFTWARE.
+*
+*************************************************************************/
+
+/* General libraries */
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+/* User defined libraries */
+#include "hd44780.h"
+#include "ringbuffer.h"
+#include "utils.h"
+#include "memory.h"
+
+#define ENABLE_LATCH 0x02
+#define BACKLIGHT_ON 0x03
+
+typedef enum {
+	LCD_STATE_IDLE,
+	LCD_STATE_CHECK_BUSY,
+	LCD_STATE_SEND_COMMAND,
+	LCD_STATE_PREPARE_COMMAND
+} lcd_fsm_state_t;
+
+volatile static lcd_fsm_state_t lcd_fsm_state = LCD_STATE_IDLE;
+
+static queue_t q;
+static queue_t* queue = NULL;
+static payload_t* _payload = NULL;
+static uint8_t _busy_flag = 0x00;
+static uint8_t _sensor_index = 0;
+static sensor_data_t _sensor_data;
+static sensor_data_t* _ptr_sensor_data = NULL; 
+
+static void _hd44780_set_to_4bit_operation(void);
+static void _hd44780_send_8bit_handler(uint8_t opcode, uint8_t instruction);
+static void _hd44780_send_8_bit_instruction(uint8_t opcode, uint8_t instruction, callback_fn callback);
+static payload_t* _hd44780_prepare_command(void);
+static void _hd44780_send_command(payload_t* payload);
+static void _hd44780_request_busy_flag(void* context);
+static void _hd44780_update_busy_flag(void* context);
+static void _hd44780_init_sensor_data(sensor_data_t* sensor_data);
+static void _hd44780_timer_init(void);
+static void _hd44780_start_timer(void);
+static void _hd44780_stop_timer(void);
+
+void hd44780_init(void) {
+	
+	_delay_ms(100); // Boot Time
+	
+	_busy_flag = 0x00;
+	_ptr_sensor_data = &_sensor_data;
+	
+	DISPLAY_DDR = 0xFF;
+	
+	queue = queue_init(&q);
+	
+	_hd44780_init_sensor_data(_ptr_sensor_data);
+	
+	_hd44780_timer_init();
+	
+	// Force 8-bit mode
+	_hd44780_send_8_bit_instruction(RWRS00, 0x30, NULL);
+	_delay_ms(5); 
+	_hd44780_send_8_bit_instruction(RWRS00, 0x30, NULL);
+	_delay_ms(1);
+	_hd44780_send_8_bit_instruction(RWRS00, 0x30, NULL);
+	_delay_ms(1);
+	
+	// Operates in 4-bit mode from here on
+	_hd44780_set_to_4bit_operation(); 
+	_delay_ms(1);
+	
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, FUNCTION_SET_4_BIT_MODE_5x8));
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, DISPLAY_OFF));
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, CLEAR_DISPLAY));
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, CURSOR_DIR_LEFT_NO_SHIFT));
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, DISPLAY_ON));
+	
+	hd44780_update();	
+	
+	_hd44780_start_timer();
+}
+
+/*
+	In order to set the HD44780 into 4-bit mode we need to send a single 8-bit command with only
+	one write operation.
+
+	HD44780 Datasheet Table 12:
+	> Sets to 4-bit operation. In this case, operation is handled as 8 bits by initialization, 
+	> and only this instruction completes with one write.
+*/
+static void _hd44780_set_to_4bit_operation(void) {
+	
+	uint8_t high_nibble;
+		
+	high_nibble = 0x20 & 0xF0;
+	
+	DISPLAY_PORT = (high_nibble | (RWRS00 << 0) | (1 << BACKLIGHT_PIN)| (1 << ENABLE_PIN));
+	DISPLAY_PORT = (high_nibble | (RWRS00 << 0) | (1 << BACKLIGHT_PIN)) & ~(1 << ENABLE_PIN);
+}
+
+static void _hd44780_send_8bit_handler(uint8_t opcode, uint8_t instruction) {
+	
+	uint8_t high_nibble;
+	uint8_t low_nibble;
+		
+	high_nibble = instruction & 0xF0;
+	low_nibble = (instruction & 0x0F) << 4;
+	
+	DISPLAY_PORT = (high_nibble | (opcode << 0) | (1 << BACKLIGHT_PIN) | (1 << ENABLE_PIN));
+	DISPLAY_PORT = (high_nibble | (opcode << 0) | (1 << BACKLIGHT_PIN)) & ~(1 << ENABLE_PIN);
+	DISPLAY_PORT = (low_nibble  | (opcode << 0) | (1 << BACKLIGHT_PIN) | (1 << ENABLE_PIN));
+	DISPLAY_PORT = (low_nibble  | (opcode << 0) | (1 << BACKLIGHT_PIN)) & ~(1 << ENABLE_PIN);
+}
+
+static void _hd44780_send_8_bit_instruction(uint8_t opcode, uint8_t instruction, callback_fn callback) {		
+	_hd44780_send_8bit_handler(opcode, instruction);
+}
+
+static payload_t* _hd44780_prepare_command(void) {
+	
+	payload_t* payload;
+	
+	payload = queue_dequeue(queue);
+	
+	if (payload == NULL) {
+		return NULL;
+	}
+		
+	return payload;
+}
+
+static void _hd44780_send_command(payload_t* payload) {
+	
+	uint8_t opcode;
+	uint8_t instruction;
+	
+	opcode = payload->hd44780.opcode;
+	
+	instruction = payload->hd44780.instruction;
+	
+	payload_free_hd44780(payload);
+	
+	_hd44780_send_8_bit_instruction(opcode, instruction, NULL);
+}
+
+static void _hd44780_request_busy_flag(void* context) {
+	_hd44780_send_8bit_handler(0xFF, RWRS10);
+}
+
+static void _hd44780_update_busy_flag(void* context) {
+	
+	// TODO: Get Busy Flag, TWDR & 0x80, LCD always responds with 0x80 (Busy)
+	_busy_flag = 0x00; 
+	
+	lcd_fsm_state = LCD_STATE_CHECK_BUSY;
+}
+
+static void _hd44780_init_sensor_data(sensor_data_t* sensor_data) {
+	
+	strcpy(sensor_data->sensors[TEMPERATURE_SENSOR].name, "Temperature");
+	strcpy(sensor_data->sensors[HUMIDITY_SENSOR].name, "Humidity");
+	strcpy(sensor_data->sensors[PRESSURE_SENSOR].name, "Pressure");
+	strcpy(sensor_data->sensors[CO2_SENSOR].name, "CO2");
+	strcpy(sensor_data->sensors[LIGHT_SENSOR].name, "Light"); 
+	strcpy(sensor_data->sensors[SOUND_SENSOR].name, "Sound"); 
+	
+	strcpy(sensor_data->sensors[TEMPERATURE_SENSOR].unit, "C");
+	strcpy(sensor_data->sensors[HUMIDITY_SENSOR].unit, "%");
+	strcpy(sensor_data->sensors[PRESSURE_SENSOR].unit, "Pa");
+	strcpy(sensor_data->sensors[CO2_SENSOR].unit, "ppm");
+	strcpy(sensor_data->sensors[LIGHT_SENSOR].unit, "lx");
+	strcpy(sensor_data->sensors[SOUND_SENSOR].unit, "dB");
+
+	// Example sensor values
+	sensor_data->sensors[TEMPERATURE_SENSOR].value = -22.05;
+	sensor_data->sensors[HUMIDITY_SENSOR].value = 55.31;
+	sensor_data->sensors[PRESSURE_SENSOR].value = 1031.2;
+	sensor_data->sensors[CO2_SENSOR].value = 400.0;
+	sensor_data->sensors[LIGHT_SENSOR].value = 150.0;
+	sensor_data->sensors[SOUND_SENSOR].value = 65.2;
+}
+
+void hd44780_set_sensor_data(sensor_index_t sensor, float value) {
+	_sensor_data.sensors[sensor].value = value;
+}
+
+void hd44780_update(void) {
+	char buffer[MAX_DISPLAY_CHAR_LENGTH + 1]; // +1 for null terminator
+	char name_buffer[MAX_DISPLAY_CHAR_LENGTH + 1];
+	char unit_buffer[MAX_DISPLAY_CHAR_LENGTH + 1];
+	char value_buffer[MAX_DISPLAY_CHAR_LENGTH + 1];
+	
+	hd44780_clear();
+
+	for (uint8_t sensor_index = _sensor_index, line = LINE1; 
+		 sensor_index < _sensor_index + LINE_COUNT; 
+		 sensor_index++, line += 0x40) {
+		
+		if (sensor_index < SENSOR_COUNT) {
+			// Convert float to string with 2 decimal places
+			size_t value_size = float_to_string(_ptr_sensor_data->sensors[sensor_index].value, value_buffer, sizeof(value_buffer), 2);
+			
+			// Calculate max allowed length for name
+			size_t unit_size = strlen(_ptr_sensor_data->sensors[sensor_index].unit);
+			size_t available_name_length =  MAX_DISPLAY_CHAR_LENGTH - (value_size + unit_size + 2); // +2 accounts for the colon(:) and space() in the final format
+			
+			snprintf(unit_buffer, sizeof(unit_buffer), "%s", _ptr_sensor_data->sensors[sensor_index].unit);
+			snprintf(name_buffer, available_name_length + 1, "%s", _ptr_sensor_data->sensors[sensor_index].name);
+			snprintf(buffer, sizeof(buffer), "%s:%s %s", name_buffer, value_buffer, unit_buffer);
+			
+			hd44780_move_cursor(line, 0);
+			hd44780_puts(buffer);
+		}
+	}
+}
+
+static void _hd44780_putc(char c) {
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS01, c));
+	_hd44780_start_timer();
+}
+
+void hd44780_puts(char* string) {
+	
+	while(*string) {
+		_hd44780_putc(*string++);
+	}
+}
+
+void hd44780_clear(void) {
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, CLEAR_DISPLAY));
+	_hd44780_start_timer();
+}
+
+void hd44780_move_cursor(uint8_t line, uint8_t offset) {
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, DDRAM_ADDR + line + offset));
+	_hd44780_start_timer();
+}
+
+void hd44780_shift_cursor_left(void) {
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, SHIFT_INSTRUCTION | SHIFT_CURSOR_LEFT));
+	_hd44780_start_timer();
+}
+
+void hd44780_shift_cursor_right(void) {
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, SHIFT_INSTRUCTION | SHIFT_CURSOR_RIGHT));
+	_hd44780_start_timer();
+}
+
+void hd44780_shift_display_left(void) {
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, SHIFT_INSTRUCTION | SHIFT_DISPLAY_LEFT));
+	_hd44780_start_timer();
+}
+
+void hd44780_shift_display_right(void) {
+	queue_enqueue(queue, payload_create_hd44780(PRIORITY_NORMAL, RWRS00, SHIFT_INSTRUCTION | SHIFT_DISPLAY_RIGHT));
+	_hd44780_start_timer();
+}
+
+void hd44780_shift_display_up(void) {
+		
+	if (_sensor_index == 0) {
+		return;
+	}
+	
+	_sensor_index -= 1;
+	
+	if (_sensor_index < 0) {
+		_sensor_index = 0;
+	}
+	
+	hd44780_update();
+}
+
+void hd44780_shift_display_down(void) {
+		
+	if (_sensor_index == SENSOR_COUNT - LINE_COUNT) {
+		return;
+	}
+	
+	_sensor_index += 1;
+	
+	if (_sensor_index > SENSOR_COUNT) {
+		_sensor_index = SENSOR_COUNT;
+	}
+	
+	hd44780_update();
+}
+
+static void _hd44780_timer_init(void) {
+	
+	// Set Timer1 to CTC mode
+	TCCR3B |= (1 << WGM32);
+	
+	// Set prescaler to 1024
+	TCCR3B |= (1 << CS32) | (1 << CS30);
+
+	// Set compare match value for 200 Hz interval (assuming 10MHz clock)
+	OCR3A = 47;
+}
+
+static void _hd44780_start_timer(void) {
+	
+	// Activate Timer only if not already enabled
+	if (TIMSK3 & (1 << OCIE3A)) {
+		// Do nothing
+	} else {
+		// Reset timer counter to 0
+		TCNT3 = 0;
+		
+		// Enable Timer1 Compare Match A interrupt
+		TIMSK3 |= (1 << OCIE3A);
+	}
+}
+
+static void _hd44780_stop_timer(void) {
+	// Disable Timer1 Compare Match A interrupt
+	TIMSK3 &= ~(1 << OCIE3A);
+}
+
+ISR(TIMER3_COMPA_vect) {
+			
+	switch(lcd_fsm_state) {
+		
+		case LCD_STATE_IDLE: {
+			
+			if (queue_empty(queue)) {
+				_hd44780_stop_timer();
+			} else {
+				lcd_fsm_state = LCD_STATE_PREPARE_COMMAND;
+				
+				_hd44780_start_timer();
+			}
+			
+			break;
+		}
+		
+		case LCD_STATE_PREPARE_COMMAND: {
+			
+			_hd44780_stop_timer();
+			
+			_payload = _hd44780_prepare_command();
+				
+			if (_payload == NULL) {
+				lcd_fsm_state = LCD_STATE_IDLE;
+			} else {
+				lcd_fsm_state = LCD_STATE_SEND_COMMAND;
+				
+				_hd44780_start_timer();
+			}		
+			
+			break;
+		}
+		
+		case LCD_STATE_SEND_COMMAND: {
+					
+			if (_payload != NULL) {
+				_hd44780_send_command(_payload);
+			} else {
+				
+			}
+			
+			_payload = NULL;
+			
+			lcd_fsm_state = LCD_STATE_CHECK_BUSY;
+			
+			break;
+		}
+		
+		case LCD_STATE_CHECK_BUSY: {
+			
+			lcd_fsm_state = LCD_STATE_PREPARE_COMMAND;
+			
+			break;
+		}
+		
+		default: {
+			break;
+		}
+	}
+}
---
+++ hd44780.h
@@ -0,0 +1,134 @@
+/*************************************************************************
+* Title		: hd44780.h
+* Author	: Dimitri Dening
+* Created	: 08.03.2025 22:07:18
+* Software	: Microchip Studio V7
+* Hardware	: Atmega2560
+* License	: MIT License
+* Usage		: see Doxygen manual
+*
+*       Copyright (C) 2025 Dimitri Dening
+*
+*       Permission is hereby granted, free of charge, to any person obtaining a copy
+*       of this software and associated documentation files (the "Software"), to deal
+*       in the Software without restriction, including without limitation the rights
+*       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*       copies of the Software, and to permit persons to whom the Software is
+*       furnished to do so, subject to the following conditions:
+*
+*       The above copyright notice and this permission notice shall be included in all
+*       copies or substantial portions of the Software.
+*
+*       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*       SOFTWARE.
+*
+*************************************************************************/
+
+#ifndef HD44780_H_
+#define HD44780_H_
+
+/* General libraries */
+
+/* User defined libraries */
+
+/* HD44780 */
+#define LINE_COUNT  0x02
+#define LINE_LENGTH 0x14
+#define LINE1	0x00
+#define LINE2	LINE1 + 0x40
+#define LINE3	LINE1 + LINE_LENGTH
+#define	LINE4 	LINE2 + LINE_LENGTH
+#define DDRAM_ADDR 0x80
+
+#define DISPLAY_DDR		DDRC
+#define DISPLAY_PORT	PORTC
+#define RS_PIN			PINC0
+#define RW_PIN			PINC1
+#define ENABLE_PIN		PINC2
+#define BACKLIGHT_PIN	PINC3
+#define DB4				PINC4
+#define DB5				PINC5
+#define DB6				PINC6
+#define DB7				PINC7
+
+/* OPCODE */
+#define RWRS00 0x00
+#define RWRS01 0x01
+#define RWRS10 0x02
+#define RWRS11 0x03
+
+/* DISPLAY CONTROL */
+#define DISPLAY_OFF     0x08
+#define DISPLAY_ON      0x0C
+#define CURSOR_ON       0x0A 
+#define BLINK_ON        0x09  
+#define CLEAR_DISPLAY   0x01
+#define RETURN_HOME     0x02
+#define BUSY_FLAG		0x80
+
+/* FUNCTION SET DEFINITION (EUROPEAN ONLY) */
+#define FUNCTION_SET_4_BIT_MODE_5x8    0x28 // 4 Bit Mode, 2 Lines, 5x8 Dots
+#define FUNCTION_SET_4_BIT_MODE_5x10   0x2C // 4 Bit Mode, 2 Lines, 5x10 Dots
+
+/* ENTRY MODE SET */
+#define CURSOR_DIR_LEFT_SHIFT     0x07
+#define CURSOR_DIR_LEFT_NO_SHIFT  0x06
+#define CURSOR_DIR_RIGHT_SHIFT    0x05
+#define CURSOR_DIR_RIGHT_NO_SHIFT 0x04
+
+/* CHARACTER GRAPHICS MODE W/ INTERNAL POWER */
+#define CHARACTER_MODE_INTERNAL_PWR    0x17
+#define CHARACTER_MODE_NO_INTERNAL_PWR 0x13
+#define GRAPHIC_MODE_NO_INTERNAL_PWR   0x1B
+#define GRAPHIC_MODE_INTERNAL_PWR      0x1F
+
+/* Moves cursor and shifts display without changing DDRAM contents */
+#define SHIFT_INSTRUCTION   0x10
+#define SHIFT_CURSOR_LEFT   0x00
+#define SHIFT_CURSOR_RIGHT  0x04
+#define SHIFT_DISPLAY_LEFT  0x08
+#define SHIFT_DISPLAY_RIGHT 0x0C
+
+/* Typdef for Display Output */
+#define MAX_DISPLAY_CHAR_LENGTH 16
+
+// Enum for named sensor indices
+typedef enum {
+	TEMPERATURE_SENSOR,
+	HUMIDITY_SENSOR,
+	PRESSURE_SENSOR,
+	CO2_SENSOR,
+	LIGHT_SENSOR,
+	SOUND_SENSOR,
+	SENSOR_COUNT // Automatically gives total number of sensors
+} sensor_index_t;
+
+typedef struct {
+	char name[MAX_DISPLAY_CHAR_LENGTH];
+	char unit[MAX_DISPLAY_CHAR_LENGTH];
+	float value;
+} sensor_t;
+
+typedef struct {
+	sensor_t sensors[SENSOR_COUNT]; // Use SENSOR_COUNT from the enum
+} sensor_data_t;
+
+void hd44780_init(void);
+void hd44780_puts(char* string);
+void hd44780_update(void);
+void hd44780_clear(void);
+void hd44780_move_cursor(uint8_t, uint8_t);
+void hd44780_shift_cursor_left(void);
+void hd44780_shift_cursor_right(void);
+void hd44780_shift_display_left(void);
+void hd44780_shift_display_right(void);
+void hd44780_shift_display_up(void);
+void hd44780_shift_display_down(void);
+void hd44780_set_sensor_data(sensor_index_t sensor, float value);
+
+#endif /* HD44780_H_ */
